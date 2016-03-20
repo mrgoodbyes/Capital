332e7a1a609a095aa27780baa3310ada164394c1
Mrgoodbyes <Mrgoodbyes@192.168.0.12>
2016-03-20 23:12:16 +0100

    Initial load

---
+++ parent/Capital-EJB/src/main/java/org/Capital/EJB/ApplicationBean.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.Capital.EJB;
+
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+
+/**
+ *
+ * @author martin
+ */
+@Stateless
+@LocalBean
+public class ApplicationBean {
+
+    public void businessMethod() {
+    }
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+
+}
