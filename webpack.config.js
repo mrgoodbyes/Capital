e5541ca7d81bf311c5c4007fe00bef267e2f375f
Viktor Hubert <viktor.hubert@eventstag.com>
2016-03-20 23:34:18 +0000

    Added the base for handling asset versions.

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,36 @@
 var path = require('path');
+var fs = require('fs');
 
 module.exports = {
-    entry: './front_src/scripts/main.js',
+    entry: {
+        main: './front_src/scripts/entries/main.js',
+        other: './front_src/scripts/entries/other.js',
+    },
     output: {
-        path: __dirname + '/parent/Capital-WEB/src/main/webapp/js',
-        filename: 'main.js'
+        path: path.join(__dirname, 'parent/Capital-WEB/src/main/webapp/js'),
+        filename: '[name].js'
     },
     module: {
         loaders: [
             {
-                // test: path.join(__dirname, 'es6'),
                 test: /.js$/,
                 loader: 'babel-loader'
             }
         ]
-    }
+    },
+    plugins: [
+        function() {
+            this.plugin('done', function(stats) {
+                // todo: improve this method to handle multiple files with possibly multiple chunks
+                var hash = stats.toJson().chunks[0].hash;
+                fs.writeFileSync(
+                    path.join(__dirname, 'parent/Capital-EJB/src/main/resources/assets.manifest.json'),
+                    JSON.stringify(stats.toJson(), null, 4)
+                    // JSON.stringify({
+                    //     'main.js': hash
+                    // }, null, 4)
+                );
+            });
+        }
+    ]
 };
